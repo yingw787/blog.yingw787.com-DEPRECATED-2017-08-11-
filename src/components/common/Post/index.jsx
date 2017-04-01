22ce6ecee7691bb3b95f237ffb028a44c74a322d
Ying Wang <yingw787@gmail.com>
2017-04-01 15:17:38 -0400

    Switched out marked to markdown-it.

--- src/components/common/Post/index.jsx
+++ src/components/common/Post/index.jsx
@@ -1,15 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { Parser } from 'html-to-react';
-import marked from 'marked';
+import MarkdownParser from 'markdown-it';
 
 const HtmlToReactParser = new Parser();
-// const MarkdownParser = new markdown();
+const md = new MarkdownParser();
 
 export default class Post extends Component {
     render() {
         return (
             <div>
-                {HtmlToReactParser.parse(marked('I am using __markdown__.'))}
+                {HtmlToReactParser.parse(md.render('markdown-it __rules__.'))}
             </div>
         );
     }
